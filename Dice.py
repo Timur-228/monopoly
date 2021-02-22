124a69c97332c683fffb68bc0206e22a65c5b1fc
Timur-228 <76957343+Timur-228@users.noreply.github.com>
2021-02-22 11:12:01 +0300

    Update Dice.py

--- Dice.py
+++ Dice.py
@@ -1,18 +1,17 @@
 
-
 from random import randint
-import Jail
+
 
 def dice(times=0):
     dice_1 = randint(1, 6)
     dice_2 = randint(1, 6)
+    print(dice_1, dice_2)
     if dice_1 == dice_2:
         if times == 2:
-            Jail.jail()
+            return False
         else:
-            dice(times+1)
+            return dice(times + 1)
     else:
         summ = dice_1 + dice_2
         return summ
-
 
